e6a367dba2c2a8fbcda7249943740d73bf3f7813
Sait Ramazan <70922350+srgul@users.noreply.github.com>
2021-02-09 22:30:34 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-login></app-login>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from '../src/components/Login/Login'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
   components: {
-    HelloWorld
+    "app-login" : Login
   }
+ 
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Login/Login.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container text-center mt-5">
+        <hr>
+        name <br>
+        <input type="text" placeholder="name" name="" id="" v-model="user.name"> <hr><br>
+        username <br>
+        <input type="text" placeholder="username" name="" id="" v-model="user.username">      
+        <hr><br>
+        <button class="btn btn-primary" @click="btnalert">
+            Submit
+        </button>
+        <br><hr>
+        {{ user.name }}
+        {{ user.username }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            user : { 
+                name: "sait",
+                username: "enes"
+            }
+        }
+    },
+    methods: {
+        btnalert() {
+            alert( this.user.name )
+        }
+    },
+    computed:{
+
+    },
+    created() {
+        
+    },
+    
+}
+</script>
+
+<style scoped>
+    .header { 
+        margin-top: 150px;
+    }
+</style>
